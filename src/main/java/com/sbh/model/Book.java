e6d83a7225985240e7fbf1c71ebb0e7693d5396e
SunLighttt <974951303@qq.com>
2018-10-18 15:28:48 +0800

    Initial commit

---
+++ src/main/java/com/sbh/model/Book.java
@@ -0,0 +1,40 @@
+package com.sbh.model;
+
+public class Book {
+    private String bname;
+    private String author;
+    private String price;
+    private String id;
+
+    public String getBname() {
+        return bname;
+    }
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
